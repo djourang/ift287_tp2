4a3905d1ba261cec8d3f7e6e7869866f14f62842
djourang <68035736+djourang@users.noreply.github.com>
2023-06-07 09:28:30 -0400

    Update Connexion.java

--- AubergeInn/src/AubergeInn/Connexion.java
+++ AubergeInn/src/AubergeInn/Connexion.java
@@ -3,145 +3,145 @@ package AubergeInn;
 import java.sql.*;
 
 /**
- * Gestionnaire d'une connexion avec une BD relationnelle via JDBC.<br><br>
+ * Gestionnaire d une connexion avec une BD relationnelle via JDBC.<br><br>
  * 
  * Cette classe ouvre une connexion avec une BD via JDBC.<br>
- * La méthode serveursSupportes() indique les serveurs supportés.<br>
+ * La methode serveursSupportes() indique les serveurs supportes.<br>
  * <pre>
- * Pré-condition
- *   Le driver JDBC approprié doit être accessible.
+ * Pre-condition
+ *   Le driver JDBC approprie doit être accessible.
  * 
  * Post-condition
- *   La connexion est ouverte en mode autocommit false et sérialisable, 
- *   (s'il est supporté par le serveur).
+ *   La connexion est ouverte en mode autocommit false et serialisable, 
+ *   (s il est supporte par le serveur).
  * </pre>
  * <br>
- * IFT287 - Exploitation de BD relationnelles et OO
+ * IFT287 - Exploitation de BD relationnelles et OO 
  * 
- * @author Marc Frappier - Université de Sherbrooke
+ * @author Marc Frappier - Universite de Sherbrooke
  * @version Version 2.0 - 13 novembre 2004
  * 
  * 
- * @author Vincent Ducharme - Université de Sherbrooke
+ * @author Vincent Ducharme - Universite de Sherbrooke
  * @version Version 3.0 - 21 mai 2016
  */
 public class Connexion
 {
-    private Connection conn;
+	private Connection conn;
 
-    /**
-     * Ouverture d'une connexion en mode autocommit false et sérialisable (si
-     * supporté)
-     * 
-     * @param serveur Le type de serveur SQL à utiliser (Valeur : local, dinf).
-     * @param bd      Le nom de la base de données sur le serveur.
-     * @param user    Le nom d'utilisateur à utiliser pour se connecter à la base de données.
-     * @param pass    Le mot de passe associé à l'utilisateur.
-     */
-    public Connexion(String serveur, String bd, String user, String pass)
-            throws IFT287Exception, SQLException
-    {
-        Driver d;
-        try
-        {
-            d = (Driver)Class.forName("org.postgresql.Driver").newInstance();
-            DriverManager.registerDriver(d);
-            
-            if (serveur.equals("local"))
-            {
-                conn = DriverManager.getConnection("jdbc:postgresql:" + bd, user, pass);
-            }
-            else if (serveur.equals("dinf"))
-            {
-                conn = DriverManager.getConnection("jdbc:postgresql://bd-info2.dinf.usherbrooke.ca:5432/" + bd + "?ssl=true&sslmode=require", user, pass);
-            }
-            else
-            {
-                throw new IFT287Exception("Serveur inconnu");
-            }
+	/**
+	 * Ouverture d une connexion en mode autocommit false et serialisable (si
+	 * supporte)
+	 * 
+	 * @param serveur Le type de serveur SQL a utiliser (Valeur : local, dinf).
+	 * @param bd      Le nom de la base de donnees sur le serveur.
+	 * @param user    Le nom d utilisateur a utiliser pour se connecter a la base de donnees.
+	 * @param pass    Le mot de passe associe a l utilisateur.
+	 */
+	public Connexion(String serveur, String bd, String user, String pass)
+			throws IFT287Exception, SQLException
+	{
+		Driver d;
+		try
+		{
+			d = (Driver)Class.forName("org.postgresql.Driver").newInstance();
+			DriverManager.registerDriver(d);
+			
+			if (serveur.equals("local"))
+			{
+				conn = DriverManager.getConnection("jdbc:postgresql:" + bd, user, pass);
+			}
+			else if (serveur.equals("dinf"))
+			{
+				conn = DriverManager.getConnection("jdbc:postgresql://bd-info2.dinf.usherbrooke.ca:5432/" + bd + "?ssl=true&sslmode=require", user, pass);
+			}
+			else
+			{
+				throw new IFT287Exception("Serveur inconnu");
+			}
 
-            // Mise en mode de commit manuel
-            conn.setAutoCommit(false);
+			// Mise en mode de commit manuel
+			conn.setAutoCommit(false);
 
-            // Mise en mode sérialisable, si possible
-            // (plus haut niveau d'integrité pour l'accès concurrent aux données)
-            DatabaseMetaData dbmd = conn.getMetaData();
-            if (dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE))
-            {
-                conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
-                System.out.println("Ouverture de la connexion en mode sérialisable :\n"
-                        + "Connecté sur la BD postgreSQL "
-                        + bd + " avec l'utilisateur " + user);
-            }
-            else
-            {
-                System.out.println("Ouverture de la connexion en mode read committed (default) :\n"
-                        + "Connecté sur la BD postgreSQL "
-                        + bd + " avec l'utilisateur " + user);
-            }
-        }
-        catch (SQLException e)
-        {
-            throw e;
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace(System.out);
-            throw new IFT287Exception("JDBC Driver non instancié");
-        }
-    }
+			// Mise en mode serialisable, si possible
+			// (plus haut niveau d integrite pour l acces concurrent aux donnees)
+			DatabaseMetaData dbmd = conn.getMetaData();
+			if (dbmd.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE))
+			{
+				conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+				System.out.println("Ouverture de la connexion en mode serialisable :\n"
+						+ "Connecte sur la BD postgreSQL "
+						+ bd + " avec l utilisateur " + user);
+			}
+			else
+			{
+				System.out.println("Ouverture de la connexion en mode read committed (default) :\n"
+						+ "Connecte sur la BD postgreSQL "
+						+ bd + " avec l utilisateur " + user);
+			}
+		}
+		catch (SQLException e)
+		{
+			throw e;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace(System.out);
+			throw new IFT287Exception("JDBC Driver non instancie");
+		}
+	}
 
-    /**
-     * Fermeture d'une connexion
-     */
-    public void fermer() throws SQLException
-    {
-        conn.rollback();
-        conn.close();
-        System.out.println("Connexion fermée " + conn);
-    }
+	/**
+	 * Fermeture d une connexion
+	 */
+	public void fermer() throws SQLException
+	{
+		conn.rollback();
+		conn.close();
+		System.out.println("Connexion fermee " + conn);
+	}
 
-    /**
-     * Commit
-     */
-    public void commit() throws SQLException
-    {
-        conn.commit();
-    }
+	/**
+	 * Commit
+	 */
+	public void commit() throws SQLException
+	{
+		conn.commit();
+	}
 
-    public void setIsolationReadCommited() throws SQLException
-    {
-        conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-    }
+	public void setIsolationReadCommited() throws SQLException
+	{
+		conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+	}
 
-    /**
-     * Rollback
-     */
-    public void rollback() throws SQLException
-    {
-        conn.rollback();
-    }
+	/**
+	 * Rollback
+	 */
+	public void rollback() throws SQLException
+	{
+		conn.rollback();
+	}
 
-    /**
-     * Retourne la Connection JDBC
-     */
-    public Connection getConnection()
-    {
-        return conn;
-    }
+	/**
+	 * Retourne la Connection JDBC
+	 */
+	public Connection getConnection()
+	{
+		return conn;
+	}
 
-    public void setAutoCommit(boolean m) throws SQLException
-    {
-        conn.setAutoCommit(false);
-    }
+	public void setAutoCommit(boolean m) throws SQLException
+	{
+		conn.setAutoCommit(false);
+	}
 
-    /**
-     * Retourne la liste des serveurs supportés par ce gestionnaire de
-     * connexions
-     */
-    public static String serveursSupportes()
-    {
-        return "local : PostgreSQL installé localement\n"
-             + "dinf  : PostgreSQL installé sur les serveurs du département\n";
-    }
+	/**
+	 * Retourne la liste des serveurs supportes par ce gestionnaire de
+	 * connexions
+	 */
+	public static String serveursSupportes()
+	{
+		return "local : PostgreSQL installe localement\n"
+			 + "dinf  : PostgreSQL installe sur les serveurs du departement\n";
+	}
 }
